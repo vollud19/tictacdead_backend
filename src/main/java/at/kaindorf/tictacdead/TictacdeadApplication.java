8c26607498297c060ecec88926f7e4da4bffc226
Allwinger <allotd19@htl-kaindorf.at>
2023-03-29 09:19:38 +0200

    First Commit

---
+++ src/main/java/at/kaindorf/tictacdead/TictacdeadApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.tictacdead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictacdeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictacdeadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/kaindorf/tictacdead/TictacdeadApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.tictacdead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictacdeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
